521f23f677a2d23368a406064e572602b41b2a81
Patrick Schroen <ps@ufotechnologies.com>
2023-01-30 20:59:22 -0500

    Update tests

--- test/index.js
+++ test/index.js
@@ -2,11 +2,11 @@ import { chdir } from 'node:process';
 import { rollup } from 'rollup';
 import test from 'ava';
 
-import { regex } from '../index.js';
+import { regex, babel, terser } from '../index.js';
 
 chdir('test');
 
-test('strip exports', async t => {
+test('regex replace', async t => {
     const bundle = await rollup({
         input: 'fixtures/basic.js',
         plugins: [regex([[/^[\r\n]+export.*/m, '']])]
@@ -16,4 +16,28 @@ test('strip exports', async t => {
     const code = output[0].code;
 
     t.is(!code.includes('export'), true);
+});
+
+test('transpile class', async t => {
+    const bundle = await rollup({
+        input: 'fixtures/basic.js',
+        plugins: [babel()]
+    });
+
+    const { output } = await bundle.generate({ format: 'es' });
+    const code = output[0].code;
+
+    t.is(code.includes('_createClass'), true);
+});
+
+test('minify', async t => {
+    const bundle = await rollup({
+        input: 'fixtures/basic.js',
+        plugins: [terser()]
+    });
+
+    const { output } = await bundle.generate({ format: 'es' });
+    const code = output[0].code;
+
+    t.is(code.trim(), 'class GrumpyCat{speak(){return this}}export{GrumpyCat};');
 });
